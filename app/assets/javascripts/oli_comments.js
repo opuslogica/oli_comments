8cc372b97efa51a8ed56511bf970b6f175b3fab5
Alex Bundy <abundy@opuslogica.com>
2015-07-09 12:27:51 -0700

    fix(comment form): Make it work on Safari.

--- app/assets/javascripts/oli_comments.js
+++ app/assets/javascripts/oli_comments.js
@@ -1,6 +1,6 @@
 $(document).on('keypress', function(e) {
-    var tag = e.target.tagName.toLowerCase();
-    if ( e.which === 13 && tag == 'textarea' && e.target.id == 'comment_body') {
-			$(e.target.closest("form")).submit();			
-		}   
+  var tag = e.target.tagName.toLowerCase();
+  if (e.which === 13 && tag == 'textarea' && e.target.id == 'comment_body') {
+    $(e.target).closest("form").submit();			
+  }
 });
